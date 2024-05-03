ad708d9fe10aaa581f843da64212b24fc8f4cd97
modolet <modolet@163.com>
2024-05-03 23:45:22 +0800

    适用于win7的wdk-alloc和wdk-log

---
+++ wdk-log/src/lib.rs
@@ -0,0 +1,42 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::{ffi::CString, format, string::ToString};
+use log::{LevelFilter, SetLoggerError};
+use wdk_sys::ntddk::DbgPrint;
+
+static LOGGER: WdkLogger = WdkLogger;
+
+pub struct WdkLogger;
+
+impl WdkLogger {
+    pub fn init(level: LevelFilter) -> Result<(), SetLoggerError> {
+        log::set_logger(&LOGGER).map(|()| log::set_max_level(level))
+    }
+}
+
+impl log::Log for WdkLogger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        metadata.level() <= log::max_level()
+    }
+
+    fn log(&self, record: &log::Record) {
+        if self.enabled(record.metadata()) {
+            let message = format!(
+                "[XX] {:<5} [{}:{}] {}\n\0",
+                record.level().to_string(),
+                record.target(),
+                record.line().unwrap_or(0),
+                record.args()
+            );
+            let message = widestring::U16CString::from_str(message).unwrap();
+            let format = CString::new("%ws").unwrap();
+            unsafe {
+                DbgPrint(format.as_ptr() as _, message.as_ptr());
+            }
+        }
+    }
+
+    fn flush(&self) {}
+}
---
+++ wdk-string/src/lib.rs
@@ -0,0 +1,35 @@
+use wdk_sys::{ntddk::RtlInitUnicodeString, PUNICODE_STRING, UNICODE_STRING};
+
+pub trait NtToString {
+    fn to_string(&self) -> alloc::string::String;
+}
+
+pub trait NtFromString {
+    fn from_string(s: &str) -> UNICODE_STRING;
+}
+
+impl NtToString for UNICODE_STRING {
+    fn to_string(&self) -> alloc::string::String {
+        unsafe {
+            widestring::U16String::from_ptr(self.Buffer, self.Length as usize / 2)
+                .to_string()
+                .unwrap()
+        }
+    }
+}
+
+impl NtFromString for UNICODE_STRING {
+    fn from_string(s: &str) -> UNICODE_STRING {
+        let wide_str = widestring::U16String::from_str(s);
+        let mut ustring = UNICODE_STRING::default();
+        unsafe { RtlInitUnicodeString(&mut ustring, wide_str.as_ptr()) };
+        ustring
+    }
+}
+
+impl NtToString for PUNICODE_STRING {
+    fn to_string(&self) -> alloc::string::String {
+        let ustring = unsafe { **self };
+        ustring.to_string()
+    }
+}
